1cb0ba46d697fa074346a43a27b80288a441b59c
crescenthw <tjguddn9899@naver.com>
2022-06-15 20:16:39 +0900

    2022-06-15 update

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import App from "./App";
 
@@ -7,9 +8,11 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  // <React.StrictMode>
-  <RecoilRoot>
-    <App />
-  </RecoilRoot>
-  /* </React.StrictMode> */
+  <React.StrictMode>
+    <Router basename={process.env.PUBLIC_URL}>
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
+    </Router>
+  </React.StrictMode>
 );
